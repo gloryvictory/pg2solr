10f36ccd570bc291ae88e7ef7a3fcccf9c55b1c0
glory <zamaraev@gmail.com>
2023-02-26 20:47:31 +0500

    2 params done

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from src.database import database, metadata, engine
 from datetime import datetime
 import ormar
@@ -8,48 +10,75 @@ from src import cfg
 from src.models import FILES_M
 
 
+
+# def str2integer(s):
+#     try:
+#         return int(s)
+#     except ValueError:
+#         return 0
+
 async def try_pg():
+
+    time1 = datetime.now()
+    print('Starting at :' + str(time1))
+
+    solr_delete()
+    if not database.is_connected:
+        await database.connect()
     cnt = 0
-    files = await FILES_M.objects.all()
-    # for file_item in files:
-    # # async for file_item in FILES_M.objects.iterate():
-    #     if cnt < 10:
-    #         assert  print(file_item.file_path)
-    #     else:
-    #         break
-    #     cnt += 1
+    # print(cnt)
+    files = await FILES_M.objects.all() #limit(10).all()
+    len_files = len(files)
+
+    for file_item in files:
+        # print(file_item.file_path)
+        print(f"{cnt} of {len_files}. Осталось {len_files - cnt}")
+        file_path = file_item.file_path
+        file_size = file_item.file_size
+        solr_add(file_path, file_size)
+        cnt += 1
+        # time3 = datetime.now()
+        # print('one:' + str(time3))
+        # print('time left: ' + str(time3 - time1))
+        # file_path = file_item.file_path
+
+    time2 = datetime.now()
+    print('Finishing at :' + str(time2))
+    print('Total time : ' + str(time2 - time1))
+    print('DONE !!!!')
 
 
-def try_solr():
+def solr_add(file_path: str = "", file_size: str = ''):
     # Create a client instance. The timeout and authentication options are not required.
     str_solr_conn = f"http://{cfg.SOLR_HOST}:{cfg.SOLR_PORT}/solr/{cfg.SOLR_COLL}"
-    print(str_solr_conn)
+    # print(str_solr_conn)
     solr = pysolr.Solr(str_solr_conn,
                        always_commit=True,
                        auth=(cfg.SOLR_USER, cfg.SOLR_PASS)
                        )  # timeout = 10,
-    # solr = pysolr.Solr('http://solr:Ghbdtn123!@localhost:8983/solr/files', always_commit=True, timeout=10)
-    str_tmp = solr.ping()
-    print(str_tmp)
-    str_tmp = solr.delete(q='*:*')
-    print(str_tmp)
+
     str_tmp = solr.add(
-        [
-            {
-                "file_path": "\\r57-vfs01.zsniigg.local\volarch\!_2019_ГПН-190900_000919_Р_048_2019-Д_01032019_СМотреть\ЗСНГП-НФ_ОтвИспРусаковПС_Межовский\2004г_Отчёт_НОВОСИБ_ТННЦ_1277_04\MAP\IZOPACH\IZOPACH_RAK_VOST\isopach_t1_b.srf",
-                "file_size": "132"
-            },
-            {
-                "file_path": "\\r57-vfs01.zsniigg.local\volarch\!_2019_ГПН-190900_000919_Р_048_2019-Д_01032019_СМотреть\ЗСНГП-НФ_ОтвИспРусаковПС_Межовский\2004г_Отчёт_НОВОСИБ_ТННЦ_1277_04\PRIL\pril_10.cdr",
-                "file_size": "234"
-            }
-        ]
+        {
+            "file_path": file_path,
+            "file_size": int(file_size)
+        }
     )
+    # print(str_tmp)
+
+
+def solr_delete():
+    str_solr_conn = f"http://{cfg.SOLR_HOST}:{cfg.SOLR_PORT}/solr/{cfg.SOLR_COLL}"
+    solr = pysolr.Solr(str_solr_conn,
+                       always_commit=True,
+                       auth=(cfg.SOLR_USER, cfg.SOLR_PASS)
+                       )  # timeout = 10,
+    str_tmp = solr.delete(q='*:*')
     print(str_tmp)
 
 
 if __name__ == "__main__":
-    try_pg()
+    asyncio.run(try_pg())
+
     # try_solr()
     # set_logger()
     # uvicorn.run("main:app", host=cfg.SERVER_HOST, port=cfg.SERVER_PORT, reload=True)
